99cea886c51ca3e24f10bc3a6c254446a9d7f5c7
Derek Maciel <dam402@jla.rutgers.edu>
2016-07-12 17:39:31 -0400

    Some modularization of main()

--- mirror.py
+++ mirror.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 
 from optparse import OptionParser
+import sys
 from ConfigParser import ConfigParser
 from datetime import datetime
 from subprocess import call
+from os.path import isfile
 
 def _get_config(file):
+    if not isfile(file):
+        raise Exception('config file not found')
+
     parser = ConfigParser()
     parser.read(file)
     return parser
@@ -13,36 +18,49 @@ def _get_config(file):
 CONFIG = _get_config("rutgers-pymirror.cfg")
 
 def main(options, args):
-    datetime_format = CONFIG.get('settings', 'datetimeformat')
     distros = list(CONFIG.sections())
     distros.remove('settings')
-    
-    if len(distros) == 0:
+
+    if options.list:
+        for distro in distros:
+            print distro
+    elif options.all:
+        for distro in distros:
+            sync(distro)
+    elif len(distros) == 0:
         print "no distros to sync"
     else:
-        for distro in distros:
-            print "Starting {} sync at {}".format(distro, 
+        for distro in args:
+            sync(distro)
+   
+
+def sync(distro): 
+    datetime_format = CONFIG.get('settings', 'datetimeformat')
+    syncurl = CONFIG.get(distro, 'url')
+    synchome = CONFIG.get(distro, 'synchome')
+    synclog = CONFIG.get(distro, 'synclog')
+
+    print "Starting {} sync at {}".format(distro, 
+        datetime.now().strftime(datetime_format))
+            
+    with open(synclog, 'a+') as distro_log:
+        ret = call( ['rsync', '--recursive', '--safe-links', '--times', 
+                     '--links', '--hardlinks', '--delete', 
+                     '--delete-excluded', '--delete-after', 
+                     '--delay-updates', '--safe-links', '-v', 
+                     syncurl, synchome] 
+                   stdout=distro_log)
+            
+        if ret == 0:
+            print "{} sync successfully completed at {}".format(distro, 
                 datetime.now().strftime(datetime_format))
-            
-            with open(CONFIG.get(distro, 'synclog'), 'a+') as distro_log:
-                ret = call( ['rsync', '--recursive', '--safe-links', '--times', 
-                             '--links', '--hardlinks', '--delete', 
-                             '--delete-excluded', '--delete-after', 
-                             '--delay-updates', '--safe-links', '-v', 
-                             CONFIG.get(distro, 'url'), 
-                             CONFIG.get(distro, 'synchome')], 
-                           stdout=distro_log)
-            
-                if ret == 0:
-                    print "{} sync successfully completed at {}".format(distro, 
-                        datetime.now().strftime(datetime_format))
-                else:
-                    print "{} sync failed at {} with returncode {}".format(
-                        distro, datetime.now.strftime(datetime_format), ret)
+        else:
+            print "{} sync failed at {} with returncode {}".format(
+                distro, datetime.now.strftime(datetime_format), ret)
 
 
 if __name__ == "__main__":
-    parser = OptionParser(usage="Usage: %prog [options] distro")
+    parser = OptionParser(usage="Usage: %prog [options] distro1 distro2 ...")
     parser.add_option("-l", "--list", action="store_true", default=False,
                       help="list all possible values for 'distro'")
     parser.add_option("-a", "--all", action="store_true", default=False,
@@ -51,9 +69,8 @@ if __name__ == "__main__":
                       help="display all output")
     
     (options, args) = parser.parse_args()
-   
-    # Display usage if no argument given
-    if len(args) == 0 and not options.all:
+
+    if len(sys.argv) == 1:
         parser.print_help()
     else:
         main(options, args)
